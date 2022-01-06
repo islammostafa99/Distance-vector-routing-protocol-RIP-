1d1ac04d763069217076ac99a6ce4837fd6cb348
IslamMostafa99 <48337171+islammostafa99@users.noreply.github.com>
2022-01-07 00:08:58 +0200

    Update routing_algo.cpp

--- routing_algo.cpp
+++ routing_algo.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// in this function, each node sends it's information to all the nodes
 void routingAlgo(vector<RoutingNode*> nd){
   for(int i=0; i<nd.size();i++){
     for(int j=0; j<nd.size();j++){
@@ -13,18 +14,23 @@ void routingAlgo(vector<RoutingNode*> nd){
 
 
 void RoutingNode::recvMsg(RouteMsg *msg) {
+  //extracting the attributes of the message (the routing table of the sender, the ip address of the sender, the ip address of the receiver)
   routingtbl* routing_table = msg->mytbl;
   string ip_sender= msg->from;
   string ip_receiver= msg->recvip;
 
   for(int i=0; i< routing_table->tbl.size();i++){
     int flag=1;
-
+    
+    //get each entry in the routing table
     RoutingEntry routing_entry = routing_table->tbl[i];
 
     for(int j=0; j<mytbl.tbl.size();j++){
+      //checking if the destination ip addresses are equal in the two entries??
       if(mytbl.tbl[j].dstip == routing_entry.dstip){
+        //checking if their is improvement in the cost??
         if(mytbl.tbl[j].cost>routing_entry.cost+1){
+          //So, update the attributes of the entry
           mytbl.tbl[j].ip_interface=ip_receiver;
           mytbl.tbl[j].nexthop=ip_sender;
           mytbl.tbl[j].cost=routing_entry.cost+1;
@@ -34,6 +40,7 @@ void RoutingNode::recvMsg(RouteMsg *msg) {
       }
     }
 
+     //checking if the flag doesn't change --> then it is the first time to insert the entry
     if(flag==1){
       RoutingEntry entry;
       entry.ip_interface=ip_receiver;
